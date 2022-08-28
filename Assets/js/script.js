6c556a7658a50243fac817bdfbb94acb5ae3239a
[josiah stelzl] <[josiahstelzl@gmail.com]>
2022-08-28 15:19:33 -0400

    worked on page refresh revention

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -2,6 +2,23 @@ var ingredientFormEl = document.getElementById("ingredient-form");
 var ingredientInputEl = document.querySelector("#ingredient-input");
 var ingredientButton = document.getElementById("ingredient-button");
 
+var formSubmitHandler = function(event) {
+    // stop page refresh
+    event.preventDefault();
+  
+    // get a value from the input
+    var ingredient = ingredientInputEl.value.trim();
+  
+    if (ingredient) {
+        drinkIngredientInfo(ingredient);
+  
+      // clear old content
+      ingredientInputEl.value = "";
+    } else {
+      console.log("enter something")
+    }
+}
+
 // api call by drink ingredient
 var drinkIngredientInfo = function(drinkIngredient) { 
     var apiUrl = "https://www.thecocktaildb.com/api/json/v1/1/search.php?i=" + drinkIngredient  + "";
@@ -15,8 +32,8 @@ var drinkIngredientInfo = function(drinkIngredient) {
     .then(function(data) {
         console.log(data);
         showDrinkIngredients(data, drinkIngredient);
-    });
-};
+    })
+}
 
 var showDrinkIngredients = function(ingredients) {
     // find out is the api returned any drinks
@@ -24,5 +41,5 @@ var showDrinkIngredients = function(ingredients) {
       console.log("yay");
       return;
     }
-};
-getDrinkNameInfo();
+}
+drinkIngredientInfo();
